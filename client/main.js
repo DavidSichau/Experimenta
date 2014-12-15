6a85f8250bab92a9ce1e79c1f11ecd391e9848c3
David Sichau <git@sichau.eu>
2014-12-15 16:07:57 +0100

    fast loader

--- client/main.js
+++ client/main.js
@@ -2,6 +2,23 @@
  * Created by dsichau on 30.10.14.
  */
 
+Meteor.startup(function() {
+    var $head, $tags, tag, _i, _len, _ref, _results;
+    $head = $('head');
+    _ref = ['meta', 'title'];
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        tag = _ref[_i];
+        $tags = $(tag);
+        $head.append($tags.clone());
+        _results.push($tags.remove());
+    }
+    //remove the loader and all styles
+    $('.spinnerLoad').remove();
+    $('style').remove();
+
+    return _results;
+});
 
 Meteor.startup(function() {
     var lang = navigator.language || navigator.userLanguage;
